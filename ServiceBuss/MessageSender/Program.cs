7d0ad31f9ed48d1ffaa99d67c944820b390b4bbe
Oscar Sotorrio <oscar.sotorrio@ascentsoftware.eu>
2021-04-16 13:36:09 +0200

    Sending messages to Service Bus

---
+++ ServiceBuss/MessageSender/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+
+namespace MessageSender
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                string text;
+
+                do
+                {
+                    Console.WriteLine("Type the message to be sent:");
+                    text = Console.ReadLine();
+                    await Execute(text);
+                    Console.WriteLine("Message sent successfully");
+                }
+                while (text.ToLower() != "exit");
+            }
+            catch (System.Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
+        }
+
+        static async Task Execute(string text)
+        {
+            var message = $"Message: {text}. Date: {DateTime.UtcNow}";
+
+            var connectionString = "Endpoint=sb://servicebus-learn-paths-salesteamapp.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=zBq34zUmK9m/jW/n8Zf18c2wOrzGghiQLpZmKpwH+no=";
+
+            var qClient = new QueueClient(connectionString, "salesteamapp");
+
+            var encodedMessage = new Message(Encoding.UTF8.GetBytes(message));
+
+            await qClient.SendAsync(encodedMessage);
+        }
+    }
+}
---
+++ ServiceBuss/Scripts/get-connection-string.ps1
@@ -0,0 +1,1 @@
+﻿az servicebus namespace authorization-rule keys list --resource-group test-learning-stuff --name RootManageSharedAccessKey --query primaryConnectionString --output tsv --namespace-name servicebus-learn-paths-salesteamapp
