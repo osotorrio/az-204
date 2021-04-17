2787502ad98337825bebbdf10d40552f7bc63b84
Oscar Sotorrio <oscar.sotorrio@ascentsoftware.eu>
2021-04-17 11:19:47 +0200

    Added basic projects for Queue Store

---
+++ QueueStorage/QueueMessageSender/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace QueueMessageSender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
