cc70d0febcf575a7ba30a371edcbcbee2cc092f7
Oscar Sotorrio <oscar.sotorrio@ascentsoftware.eu>
2021-04-17 12:49:05 +0200

    Coded sending and retriving messages

--- QueueStorage/QueueMessageReceiver/Program.cs
+++ QueueStorage/QueueMessageReceiver/Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Azure.Storage.Queues;
+using Azure.Storage.Queues.Models;
 
 namespace QueueMessageReceiver
 {
     class Program
     {
+        const string StorageAccountConnectionString = "DefaultEndpointsProtocol=https;EndpointSuffix=core.windows.net;AccountName=oscarlearning2021;AccountKey=Lw+cAi71GYQS6TzHhPN6fWN6d8a3pPSGWQ57GukZ2KRZc+OcIFq81EPoBPOmSuuUk8bf/ekjcMYNgxF4PwVoaQ==";
+
+        const string QueueName = "testqueue";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Application running...");
+            GetQueueMessageAsync().GetAwaiter().GetResult();
+            Console.WriteLine("Application finished!!");
+        }
+
+        static async Task GetQueueMessageAsync()
+        {
+            QueueClient queueClient = new QueueClient(StorageAccountConnectionString, QueueName);
+
+            //QueueMessage[] retrievedMessage = await queueClient.ReceiveMessagesAsync();
+            //Console.WriteLine($"Retrieved message with content '{retrievedMessage[0].MessageText}'");
+
+            //await queueClient.DeleteMessageAsync(retrievedMessage[0].MessageId, retrievedMessage[0].PopReceipt);
+            //Console.WriteLine($"Deleted message: '{retrievedMessage[0].MessageText}'");
+
+            await queueClient.DeleteAsync();
+            //Console.WriteLine($"Deleted queue: '{queueClient.Name}'");
         }
     }
 }
--- QueueStorage/QueueMessageSender/Program.cs
+++ QueueStorage/QueueMessageSender/Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Azure.Storage.Queues;
 
 namespace QueueMessageSender
 {
     class Program
     {
+        const string StorageAccountConnectionString = "DefaultEndpointsProtocol=https;EndpointSuffix=core.windows.net;AccountName=oscarlearning2021;AccountKey=Lw+cAi71GYQS6TzHhPN6fWN6d8a3pPSGWQ57GukZ2KRZc+OcIFq81EPoBPOmSuuUk8bf/ekjcMYNgxF4PwVoaQ==";
+
+        const string QueueName = "testqueue";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Write the message to be sent to the queue:");
+            var message = Console.ReadLine();
+            CreateQueueMessageAsync(message).GetAwaiter().GetResult();
+            Console.WriteLine("Message sent to the queue!!");
+        }
+
+        static async Task CreateQueueMessageAsync(string message)
+        {
+            QueueClient queueClient = new QueueClient(StorageAccountConnectionString, QueueName);
+
+            await queueClient.CreateIfNotExistsAsync();
+
+            if (await queueClient.ExistsAsync())
+            {
+                Console.WriteLine($"Queue '{queueClient.Name}' created");
+            }
+            else
+            {
+                Console.WriteLine($"Queue '{queueClient.Name}' exists");
+            }
+
+            await queueClient.SendMessageAsync(message);
         }
     }
 }
